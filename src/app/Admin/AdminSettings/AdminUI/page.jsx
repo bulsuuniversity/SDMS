de18539c24bdef0d8f3eeeea8295a6430ecdd3db
bulsuuniversity <144808382+bulsuuniversity@users.noreply.github.com>
2023-10-22 17:30:57 +0800

    aa

--- src/app/Admin/AdminSettings/AdminUI/page.jsx
+++ src/app/Admin/AdminSettings/AdminUI/page.jsx
@@ -15,7 +15,7 @@ const Page = () => {
             <div className="grid p-6 w-8/12">
                 <div className="flex gap-4">
                     <p className="font-bold">Manage home page Images:</p>
-                    <div className="flex items-center justify-center indent-9 ml-8">
+                    <div className="flex items-center justify-center indent-9 ml-20">
                         <button onClick={() => setOpen("carousel")} className="bg-red-800 text-white py-1 px-6">Manage</button>
                     </div>
                 </div>
