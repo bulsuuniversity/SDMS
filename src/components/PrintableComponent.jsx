a7a5ac2ded00023088f8c78ebdaf3e2c08cf9d8f
AaronAnablon <121420916+AaronAnablon@users.noreply.github.com>
2023-11-04 18:14:25 +0800

    aa

--- src/components/PrintableComponent.jsx
+++ src/components/PrintableComponent.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const PrintableComponent = React.forwardRef((content, ref) => {
+const PrintableComponent = React.forwardRef(({ content }, ref) => {
   const header = ["NUMBER", "ID NUMBER", "EMAIL", "NAME", "STATUS"]
+
+  const handleGetData = async () => {
+    startLoading()
+    try {
+      const response = await axios.get(`${url}/api/studentAccount`, { headers });
+      setFilterData(response.data)
+      stopLoading()
+    } catch (err) {
+      console.log(err);
+      stopLoading()
+    }
+  }
+
+  const data = filterData &&
+    Object.values(filterData).filter(report => {
+      const searchCondition = search ?
+        report.name && (report.name.toLowerCase()).includes(search.toLowerCase()) : true;
+      const yearLevelCondition = !yearLevel || report && report.yearLevel && report.yearLevel.includes(yearLevel);
+      const collegeCondition = !college || report && report.college === college;
+      return collegeCondition && yearLevelCondition && searchCondition;
+    });
+
   return (
     <div ref={ref}>
       <div className="m-10">
