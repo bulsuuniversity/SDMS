262d34edb3734a177e84b9887b08cf42c31e42bf
AaronAnablon <121420916+AaronAnablon@users.noreply.github.com>
2023-11-05 13:13:12 +0800

    aa

--- src/components/PrintableCert.jsx
+++ src/components/PrintableCert.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 
 const PrintableCert = React.forwardRef((content, ref) => {
-
+    const [studentName, setStudentName] = useState('');
+    const [disciplinaryAction, setDisciplinaryAction] = useState('');
+    const [agreementText, setAgreementText] = useState('');
+    
     return (
-        <div ref={ref} className='w-full h-full p-10'>
-            <h1>Your Downloadable Content</h1>
-            <p>This is the content of the downloadable div.</p>
+        <div className='w-full h-full p-10'>
+            <h1>Student Discipline Agreement</h1>
+            <form onSubmit={handleSubmit}>
+                <div ref={ref}>
+                    <label>
+                        Student Name:
+                        <input type="text" value={studentName} onChange={(e) => setStudentName(e.target.value)} />
+                    </label>
+                    <br />
+                    <label>
+                        Disciplinary Action Taken:
+                        <input type="text" value={disciplinaryAction} onChange={(e) => setDisciplinaryAction(e.target.value)} />
+                    </label>
+                    <br />
+                    <label>
+                        Agreement Text:
+                        <textarea value={agreementText} onChange={(e) => setAgreementText(e.target.value)} />
+                    </label>
+                    <br />
+                </div>
+                <button type="submit">Submit Agreement</button>
+            </form>
         </div>
     );
 });
