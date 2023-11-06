36f5236f74fc3addf71028224cdbe6b7ee153082
AaronAnablon <121420916+AaronAnablon@users.noreply.github.com>
2023-11-06 11:52:40 +0800

    ll

--- src/app/api/AdminAccount/route.js
+++ src/app/api/AdminAccount/route.js
@@ -4,20 +4,17 @@ import { NextResponse } from "next/server"
 
 export const GET = async () => {
     try {
-      const posts = await prisma.student.findMany({
-        where: {
-          role: "admin"
-        }
-      })
-  
-      return NextResponse.json(posts, {
-        headers: {
-          "revalidate": "0" 
-        }
-      });
+        const posts = await prisma.student.findMany({
+            where: {
+                role: "admin"
+            }
+        })
+
+        return NextResponse.json(posts);
     } catch (err) {
-      console.log(err)
-      return NextResponse.json({ message: "GET Error", err }, { status: 500 })
+        console.log(err)
+        return NextResponse.json({ message: "GET Error", err }, { status: 500 })
     }
-  }
-  
+}
+
+export const revalidate = 0;
