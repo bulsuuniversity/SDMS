70d2bb700e77d93189ceebdf3b81cf98dca0e9d3
AaronAnablon <121420916+AaronAnablon@users.noreply.github.com>
2023-11-12 12:25:21 +0800

    ss

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -68,12 +68,12 @@ const Blog = ({ images }, ref) => {
     return (
         <div ref={ref} className="w-full md:px-0 px-12 h-96 md:h-screen">
             <div className="flex justify-center items-center">
-                <div className="w-3/4 pt-5 pb-0 sm:pb-20 lg:pb-4">
+                <div className="w-3/4">
                     <Slider {...settings}>
                         {carousel ?
                             carousel.map((image, index) => (
                                 <div key={index} className="grid items-center justify-center">
-                                    <div className="m-auto lg:h-[44rem] 2xl:w-[52rem] lg:w-[48rem] md:h-[22rem] h-52 object-fit w-60 md:w-[32rem]">
+                                    <div className="m-auto lg:h-[44rem] 2xl:w-[52rem] lg:w-[48rem] md:h-3/4 h-52 object-fit w-60 md:w-[32rem]">
                                         <Image src={image.image} width={900} height={700} className="md:pt-24 pt-10 object-contain" alt={index} />
                                     </div>
                                 </div>
